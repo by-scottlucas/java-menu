fd5292384e34a0751793d049dcdc1dd6ca7f5dba
Lucas S <Lucasluke307@gmail.com>
2023-10-26 18:32:47 -0300

    Updates

--- Menu/Menu.java
+++ Menu/Menu.java
@@ -1,111 +1,40 @@
-package Menu;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Classe base representando um sanduiche
-class Sanduiche {
-    public String nome;
-    private double preco;
-
-    public Sanduiche(String n, double p) {
-        this.nome = n;
-        this.preco = p;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void desc() {
-        System.out.println("Temperatura em que é servido.");
-    }
-
-    public void setPreco(double p) {
-        this.preco = p;
-    }
-
-}
-
-// Subclasse que herda de um sanduiche
-class Frio extends Sanduiche {
-
-    public Frio(String n, double p) {
-        super(n, p);
-    }
-
-    @Override
-    public void desc() {
-        System.out.println(" | Servido Frio");
-    }
-
-}
-
-// Subclasse que herda de um sanduiche
-class Quente extends Sanduiche {
-
-    public Quente(String n, double p) {
-        super(n, p);
-    }
-
-    @Override
-    public void desc() {
-        System.out.println(" | Servido Quente");
-    }
-
-}
-
-// Classe principal
 public class Menu {
 
     public static void main(String[] args) {
 
-        System.out.println("------------------------------------------------");
-        System.out.println("-------- T I P O S  D E  L A N C H E S ---------");
-        System.out.println("------------------------------------------------");
+        System.out.println("-------- T I P O S  D E  L A N C H E S ---------\n");
 
         // Adicionando um Array
         List<Sanduiche> sanduiches = new ArrayList<>();
-        sanduiches.add(new Frio("* Queijo", 8.00));
-        sanduiches.add(new Frio("* Presunto e Queijo", 10.00));
-        sanduiches.add(new Frio("* Peito de Peru", 12.00));
+        sanduiches.add(new XSalada("X-Salada |", " Quente | R$", 10.00));
+        sanduiches.add(new XTudo("X-Tudo |", " Quente | R$", 30.00));
 
-        sanduiches.add(new Quente("* X-Salada", 15.00));
-        sanduiches.add(new Quente("* X-Churrasco", 18.00));
-        sanduiches.add(new Quente("* X-Tudo", 25.00));
-
-        // Filtro para listar os Nomes, Temperatura e Preço dos Lanches Frios
-        List<Sanduiche> frios = sanduiches.stream()
-                .filter(sanduiche -> sanduiche instanceof Frio)
+        // Filtro para listar o Nome, Temperatura e Preço do X-Salada
+        List<Sanduiche> xsalada = sanduiches.stream()
+                .filter(sanduiche -> sanduiche instanceof XSalada)
                 .collect(Collectors.toList());
 
-        System.out.println("LANCHES FRIOS");
-        System.out.println("------------------------------------------------");
-
-        frios.forEach(sanduiche -> {
-            System.out.print(sanduiche.getNome() + " | R$ " + sanduiche.getPreco());
-            sanduiche.desc();
+        xsalada.forEach(sanduiche -> {
+            System.out.print(sanduiche.getNome() + sanduiche.getTemperatura() + sanduiche.getPreco());
+            sanduiche.descricao();
         });
 
-        // Filtro para listar os Nomes, Temperatura e Preço dos Lanches Quentes
-        List<Sanduiche> quentes = sanduiches.stream()
-                .filter(sanduiche -> sanduiche instanceof Quente)
+        // Filtro para listar o Nome, Temperatura e Preço do X-Tudo
+        List<Sanduiche> xtudo = sanduiches.stream()
+                .filter(sanduiche -> sanduiche instanceof XTudo)
                 .collect(Collectors.toList());
 
         System.out.println("------------------------------------------------");
-        System.out.println("LANCHES QUENTES");
-        System.out.println("------------------------------------------------");
 
-        quentes.forEach(sanduiche -> {
-            System.out.print(sanduiche.getNome() + " | R$ " + sanduiche.getPreco());
-            sanduiche.desc();
+        xtudo.forEach(sanduiche -> {
+            System.out.print(sanduiche.getNome() + sanduiche.getTemperatura() + sanduiche.getPreco());
+            sanduiche.descricao();
         });
-        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------\n");
     }
 
 }
---
+++ Menu/XSalada.java
@@ -0,0 +1,12 @@
+public class XSalada extends Sanduiche {
+
+    public XSalada(String n, String t, double p) {
+        super(n, t, p);
+    }
+
+    @Override
+    public void descricao() {
+        System.out.println(" | Ingredientes: Pão, Hambúrguer, Queijo, Presunto, Alface e Tomate.");
+    }
+
+}
